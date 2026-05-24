8a87a48cbf501228cefe80a864617be87b66b15d
ohamawi <138605125+ohamawi@users.noreply.github.com>
2026-05-24 17:12:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -755,5 +755,7 @@ def logout():
 # RUN FLASK APP
 # =========================================================
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+app.run(
+    host="0.0.0.0",
+    port=int(os.environ.get("PORT", 5000))
+)
